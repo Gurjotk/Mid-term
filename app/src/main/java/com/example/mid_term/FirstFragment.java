f7cb353a1002c147b23211f737c6b07deb7cebdb
Gurjot <gurjotkaur06255@gmail.com>
2020-03-05 16:44:21 -0500

    firstfragment is committed

--- app/src/main/java/com/example/mid_term/FirstFragment.java
+++ app/src/main/java/com/example/mid_term/FirstFragment.java
@@ -7,58 +7,72 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FirstFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FirstFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class FirstFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    //private static final String ARG_PARAM1 = "param1";
+   // private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    //private String mParam1;
+ //   private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
-
+    private NavController nav;
+    ArrayList<ConsolidatedWeather> parray;
+    RecycleAdapter adapter;
     public FirstFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FirstFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FirstFragment newInstance(String param1, String param2) {
-        FirstFragment fragment = new FirstFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        nav= Navigation.findNavController(getActivity(),R.id.nav_host_fragment_container);
+        // btn_go= view.findViewById(R.id.firstFragment);
+        //btn_go.setOnClickListener(new View.OnClickListener() {
+        //    @Override
+        //  public void onClick(View v) {
+        //    nav.navigate(R.id.secondFragment);
+
+        //  }
+        // });
+        DataServices service = RetrofitClientInstance.getRetrofitInstance().create(DataServices.class);
+        Call<weather> call=service.getAllWeather();
+        call.enqueue(new Callback<weather>() {
+            @Override
+            public void onResponse(Call<weather> call, Response<weather> response) {
+                weather weather = response.body();
+                try {
+                    parray = new ArrayList<>(weather.getConsolidatedWeather());
+                    generateView(parray,view);
+                } catch (NullPointerException e) {
+                    e.printStackTrace();
+                }
+            }
+            @Override
+            public void onFailure(Call<weather> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void generateView(ArrayList<ConsolidatedWeather> parray, View view) {
     }
 
     @Override
@@ -68,42 +82,13 @@ public class FirstFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_first, container, false);
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
